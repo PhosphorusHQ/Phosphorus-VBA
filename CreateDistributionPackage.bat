194c2c965c2206d965a6cc64fc74401b234ca9d3
PhosphorusHQ <PhosphorusHQ@outlook.com>
2026-06-17 13:41:49 +0100

    Create a Distribution Package With a SHA256 Checksum

---
+++ CreateDistributionPackage.bat
@@ -0,0 +1,11 @@
+del PhosphorusPackage.tar
+del PhosphorusPackage.tar.SHA256.txt
+
+tar -caf PhosphorusPackage.tar ".\src"
+tar -rf PhosphorusPackage.tar Phosphorus.xlam pPath.xlam pUnit.xlam Phosphorus Externals.xlam PhosphorEssence.xlam 
+tar -rf PhosphorusPackage.tar "Phosphorus Tests.xlam" "PhosphorEssence - Examples.xlam" "pUnit Tests - pUnit.xlam"  "pUnit Tests - pPath.xlam" 
+tar -rf PhosphorusPackage.tar "pUnit Test Runs - pUnit.xlam" "pUnit Test Runs - pPath.xlam"
+tar -rf PhosphorusPackage.tar ".\Tests"
+tar -rf PhosphorusPackage.tar LogReader.xlsx
+
+certutil -hashfile PhosphorusPackage.tar SHA256 > PhosphorusPackage.tar.SHA256.txt

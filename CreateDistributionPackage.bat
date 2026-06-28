ab072c231428fb94251aff7e44b3a8f2ae4bccdc
PhosphorusHQ <PhosphorusHQ@outlook.com>
2026-06-28 15:21:01 +0100

    Added images folder to Distribution Package

--- CreateDistributionPackage.bat
+++ CreateDistributionPackage.bat
@@ -5,6 +5,7 @@ tar -caf PhosphorusPackage.tar ".\src"
 tar -rf PhosphorusPackage.tar Phosphorus.xlam pPath.xlam pUnit.xlam Phosphorus Externals.xlam PhosphorEssence.xlam 
 tar -rf PhosphorusPackage.tar "Phosphorus Tests.xlam" "PhosphorEssence - Examples.xlam" "pUnit Tests - pUnit.xlam"  "pUnit Tests - pPath.xlam" 
 tar -rf PhosphorusPackage.tar "pUnit Test Runs - pUnit.xlam" "pUnit Test Runs - pPath.xlam"
+tar -rf PhosphorusPackage.tar ".\images"
 tar -rf PhosphorusPackage.tar ".\Tests"
 tar -rf PhosphorusPackage.tar LogReader.xlsx
 tar -rf PhosphorusPackage.tar LICENSE

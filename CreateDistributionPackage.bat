ac465bca0e5247c3f22bf52f5e3c2b10946effb9
PhosphorusHQ <PhosphorusHQ@outlook.com>
2026-06-30 14:28:25 +0100

    Started a pPath Cribsheet & Added Docs to Distribution Package

--- CreateDistributionPackage.bat
+++ CreateDistributionPackage.bat
@@ -2,7 +2,8 @@ del PhosphorusPackage.tar
 del PhosphorusPackage.tar.SHA256.txt
 
 tar -caf PhosphorusPackage.tar ".\src"
-tar -rf PhosphorusPackage.tar Phosphorus.xlam pPath.xlam pUnit.xlam Phosphorus Externals.xlam PhosphorEssence.xlam 
+tar -caf PhosphorusPackage.tar ".\docs"
+tar -rf PhosphorusPackage.tar Phosphorus.xlam pPath.xlam pUnit.xlam Phosphorus Externals.xlam PhosphorEssence.xlam
 tar -rf PhosphorusPackage.tar "Phosphorus Tests.xlam" "PhosphorEssence - Examples.xlam" "pUnit Tests - pUnit.xlam"  "pUnit Tests - pPath.xlam" 
 tar -rf PhosphorusPackage.tar "pUnit Test Runs - pUnit.xlam" "pUnit Test Runs - pPath.xlam"
 tar -rf PhosphorusPackage.tar ".\images"
